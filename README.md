cfc4e623fe7b160d0da86ba26cc094d2a8febc8b
Luca <lucabaronih@gmail.com>
2018-11-17 14:35:47 +0100

    Add test cases

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # expression-parser
 A parser of mathematical expressions.
 Supported operations are addition, subtraction, multiplication and division of integers with the use of parentheses.
+
+# usage
+Run `npm run-script transpile-main` to convert ts code to js, then run `npm run-script test` to test it.
+
--- tests/mainSpec.js
+++ tests/mainSpec.js
@@ -4,6 +4,27 @@ const validExpression = mainFunctions.validExpression;
 const solver = mainFunctions.solver;
 
 describe('Check valid expressions', () => {
+
+  it('Evaluates an empty string as valid', () => {
+    const expr1 = '';
+    expect(validExpression(expr1)).toBe(true);
+  })
+
+  it('Evaluates a single integer as valid', () => {
+    const expr1 = '10';
+    expect(validExpression(expr1)).toBe(true);
+  })
+
+  it('Evaluates a single negative as valid', () => {
+    const expr1 = '-8';
+    expect(validExpression(expr1)).toBe(true);
+  })
+
+  it('Evaluates a single integer inside parentheses as valid', () => {
+    const expr1 = '(1)';
+    expect(validExpression(expr1)).toBe(true);
+  })
+
   it('Evaluates the sum of two integers as valid', () => {
     const expr1 = '1 + 1';
     expect(validExpression(expr1)).toBe(true);
@@ -13,4 +34,5 @@ describe('Check valid expressions', () => {
     const expr2 = '1 -- 2';
     expect(validExpression(expr2)).toBe(false);
   })
+
 });
