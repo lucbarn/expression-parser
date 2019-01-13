e7925ef6eb8360ae9078fdc75e38076d0ba6cca7
Luca <lucabaronih@gmail.com>
2019-01-13 21:20:36 +0100

    Add basic tests for solver function

--- tests/mainSpec.js
+++ tests/mainSpec.js
@@ -56,13 +56,42 @@ describe('Check valid expressions', () => {
   })
 
   it('Evaluates two consecutive operators as invalid', () => {
-    const expr2 = '1 -- 2';
-    expect(validExpression(expr2)).toBe(false);
+    const expr1 = '1 -- 2';
+    expect(validExpression(expr1)).toBe(false);
   })
 
   it('Evaluates a trailing sign as invalid', () => {
-    const expr2 = '10 + 2 +';
-    expect(validExpression(expr2)).toBe(false);
+    const expr1 = '10 + 2 +';
+    expect(validExpression(expr1)).toBe(false);
+  })
+
+});
+
+describe('Check results', () => {
+
+  it('Evaluates a single integer as equal to itself', () => {
+    const expr1 = '3';
+    expect(solver(expr1)).toBe(3);
+  })
+
+  it('Evaluates a sum', () => {
+    const expr1 = '10 + 12';
+    expect(solver(expr1)).toBe(10 + 12);
+  })
+
+  it('Evaluates a subtraction', () => {
+    const expr1 = '-2 - 5';
+    expect(solver(expr1)).toBe(-2 - 5);
+  })
+
+  it('Evaluates a multiplication', () => {
+    const expr1 = '2 * (-2)';
+    expect(solver(expr1)).toBe(2 * (-2));
+  })
+
+  it('Evaluates a division', () => {
+    const expr1 = '-10 / (-2)';
+    expect(solver(expr1)).toBe(-10 / (-2));
   })
 
 });
